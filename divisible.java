60ff4feffc9ae980beb44f22a5a4f3ba7505f0f8
Dharma raju akula <71425749+Dharmarajuakula@users.noreply.github.com>
2021-07-23 11:26:12 +0530

    Update divisible.java

--- divisible.java
+++ divisible.java
@@ -3,32 +3,26 @@ class divisibility
 {
 public static void main(String args[])
 {
-ArrayList<Integer> n=new ArrayList<Integer>();
+HashMap<Integer,Integer> n=new HashMap<Integer,Integer>();
 Scanner sc=new Scanner(System.in);
-System.out.println("Enter arraylist size");
 int five=0;
 int three=0;
-int a=sc.nextInt();
-for(int i=0;i<a;i++)
+Integer m[]={12,5,10,20,9,3,6,7,8,11};
+for(int i=0;i<m.length;i++)
 {
-n.add(sc.nextInt());
+if(n.containsKey(m[i]))
+n.put(m[i],n.get(m[i])+1);
+else
+n.put(m[i],1);
 }
-System.out.println("array list"+n);
-for(int j=0;j<a;j++)
+for(Map.Entry<Integer,Integer>i:n.entrySet())
 {
-int m=n.get(j);
-if(m%5==0)
-{
+if(i.getKey()%5==0)
 five=five+1;
-}
-if(m%3==0)
-{
+else if(i.getKey()%3==0)
 three=three+1;
-}
-
 }
 System.out.println("5="+five);
 System.out.println("3="+three);
-
 }
 }
